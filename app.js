23261babbfd23c46498844bb1a7225b42f445244
Hrushikesh Nangare <hrushikesh.nangare@fallonsoftware.com>
2017-03-11 18:37:46 +0530

    Submission of assignment
    Submission of assignment

---
+++ app.js
@@ -0,0 +1,7 @@
+var address = {city:"Pune",state:"Maharashtra",pincode:411043}
+var emplObj1 = {name:"Hrushikesh",age:25,salary:50000,address:address}
+var emplObj2 = {name:"Dipam",age:24,salary:40000,address:address}
+var emplObj3 = {name:"Deb",age:23,salary:30000,address:address}
+var emplObj4 = {name:"Ravi",age:22,salary:20000,address:address}
+var emplObj5 = {name:"Vasant",age:21,salary:10000,address:address}
+var Employee = [emplObj1,emplObj2,emplObj3,emplObj4,emplObj5]
